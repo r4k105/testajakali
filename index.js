e3cf06219e3d7c367a37412e6c7790227d9037a9
r4k105 <ryuki0608@gmail.com>
2025-06-20 08:22:21 +0700

    Commit lagi

--- index.js
+++ index.js
@@ -3,3 +3,17 @@ import mysql2 from 'mysql2';
 
 
 const app = express();
+const port = 9090;
+
+
+app.get('/', (req,res) => {
+    res.send("Hello from the world");
+});
+
+app.get('users/:userID/books/:bookID', (req,res) => {
+    res.send(`User ID: ${req.params.userID}, Book ID: ${req.params.bookID}`);
+});
+
+app.listen(port, () => {
+    console.log(`Express Server running at http://localhost:${port}`);
+});
