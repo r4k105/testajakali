44fdf09f28bb91c9ef5e50d8301f887441dbe469
r4k105 <38898877+r4k105@users.noreply.github.com>
2025-06-20 16:37:44 +0700

    Create README.md

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,6 +10,16 @@ app.get('/', (req,res) => {
     res.send('Hallo ini dari server test-git')
 })
 
+
+app.get('/about', (req,res) => {
+    res.send(`Ini AKu yg lagi berjalan pada server lokal dengan memakai port ${port}`)
+})
+
+app.get('*catchall', (req,res) => {
+    res.status(404).send('Error 404, link tidak bisa ditemukan!')
+})
+
+
 app.listen(port, () => {
     console.log(`Server http://localhost:${port} sedang jalan...`)
 })
