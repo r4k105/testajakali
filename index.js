02e9dd3210933cee4f33ad5508df38af39b15aec
r4k105 <ryuki0608@gmail.com>
2025-06-20 09:04:20 +0700

    Commit for change index.js

--- index.js
+++ index.js
@@ -14,6 +14,14 @@ app.get('users/:userID/books/:bookID', (req,res) => {
     res.send(`User ID: ${req.params.userID}, Book ID: ${req.params.bookID}`);
 });
 
+app.get('/about', (req,res) => {
+    res.send("this about on api express")
+})
+
+app.get('*catchall', (req,res) => {
+    res.status(400).send("404 error not found")
+})
+
 app.listen(port, () => {
     console.log(`Express Server running at http://localhost:${port}`);
 });
